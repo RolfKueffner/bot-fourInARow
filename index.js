00432cb291c8eb859aac89b756f346f2b91a812f
Rolf Küffner <rolfkueffner@t-online.de>
2017-03-20 11:23:14 +0100

    testing  inserting balls

--- index.js
+++ index.js
@@ -15,15 +15,83 @@ var Module = function (bot) {
   };
   this.commands = {};
 
+  var gametester;
+  var opponent;
+  var board = [[]];
+  var bottomRow = [":one:", ":two:", ":three:", ":four:", ":five:", ":six:", ":seven:"];
+  var userTurn;
+  var player;
+
   this.commands.connectfour = function(channel, args, user) {
-    var board= [[]];
-    for (i=0; i<6;i++){
-      for(j=0; j<7;i++){
-        board [i,j] = ":white_circle:";
+    if (gametester === 1){
+      bot.postMessage(channel, "Wait for the other game to end "+user.name);
+    }
+    else {
+      gametester = 1;
+      opponent = args.substring(1, args.length);
+      // todo if(opponent != _.find), timeout
+      userTurn = opponent;
+      player = user.name;
+      for (i = 0; i < 6; i++) {
+        for (j = 0; j < 7; i++) {
+          board [i, j] = ":white_circle:";
+        }
+      }
+      bot.postMessage(channel, board +"\n"+ bottomRow + "\n " + userTurn + ", your turn. Choose with !column number.");
+    }
+  };
+
+  this.commands.column = function(channel, args, user) {
+    if(gametester != 1){
+      bot.postMessage(channel, "No game running!");
+    }
+    else{
+      var column = args;
+      if(!column){
+        bot.postMessage(channel, "no valid column");
+      }
+      else{
+        switch(column) {
+          case 1:
+            break;
+          case 2:
+            break;
+          case 3:
+            break;
+          case 4:
+            break;
+          case 5:
+            break;
+          case 6:
+            break;
+          case 7:
+            break;
+          default:
+            column = -1;
+        }
+      }
+      if (column === -1){
+        bot.postMessage(channel, "no valid column");
+      }
+      else{
+        for (i = 5; i >= 0; i--){
+          if(board[i,column] === ":white_circle:"){
+            if(userTurn === opponent){
+              board[i,column] = ":red_circle:";
+              userTurn = player;
+            }
+            else{
+              board[i,column] = ":large_blue_circle:";
+              userTurn = opponent;
+            }
+            break;
+          }
+          else{
+            bot.postMessage(channel, "no valid column");
+          }
+        }
       }
     }
-    bot.postMessage(channel, board);
-
   };
 };
 
