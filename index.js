32f9b54d6c0cc504747942407008bfa799b916cb
Rolf Küffner <rolfkueffner@t-online.de>
2017-03-20 10:14:36 +0100

    test

--- index.js
+++ index.js
@@ -9,52 +9,15 @@ var Module = function (bot) {
   this.allowedChannels = [];
   this.help = function () {
     return {
-      "generatepassword": "Generate passwords"
+      "connectfour": "Play connect four vs someone"
 
     };
   };
   this.commands = {};
+  var board = ":white_circle: :white_circle: :white_circle: :white_circle: :white_circle: :white_circle: :white_circle: \n:white_circle: :white_circle: :white_circle: :white_circle: :white_circle: :white_circle: :white_circle: \n:white_circle: :white_circle: :white_circle: :white_circle: :white_circle: :white_circle: :white_circle: \n:white_circle: :white_circle: :white_circle: :white_circle: :white_circle: :white_circle: :white_circle: \n:white_circle: :white_circle: :white_circle: :white_circle: :white_circle: :white_circle: :white_circle: \n:white_circle: :white_circle: :white_circle: :white_circle: :white_circle: :white_circle: :white_circle:"
+  this.commands.connectfour = function(channel, args, user) {
+    bot.postMessage(channel, board);
 
-  this.commands.generatepassword = function(channel, args, user) {
-
-    var splittedText = args.split(" ");
-    var possibleInput = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    var possibleInputS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!()[]{}.<>@#$%=&/?ß§";
-    var passArray = [];
-    var response;
-    var length = 10;
-    var anzahl = 5;
-
-    if(splittedText[0] && splittedText[0].length != 0){
-      anzahl = splittedText[0];
-    }
-    if (splittedText[0] && splittedText[0].length != 0){
-      length = splittedText[1];
-    }
-    if (splittedText[2] && splittedText[2].toUpperCase() === "S"){
-      for (i = 0; i <= anzahl - 1; i++) {
-        var password = "";
-        for (j = 1; j <= length; j++) {
-          password += possibleInputS.charAt(Math.floor(Math.random() * possibleInputS.length));
-        }
-        passArray[i] = password;
-      }
-    }
-    else{
-      for (i = 0; i <= anzahl - 1; i++) {
-        var password = "";
-        for (j = 1; j <= length; j++) {
-          password += possibleInput.charAt(Math.floor(Math.random() * possibleInput.length));
-        }
-        passArray[i] = password;
-      }
-    }
-    var passwords = "";
-    for (i = 0; i < passArray.length; i++){
-      passwords += "\n " + passArray[i];
-    }
-    response = passwords;
-    bot.postMessage(channel, response);
   };
 };
 
